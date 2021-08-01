5b5e96569209d15d4cebc9518ca91e6d3e019ec8
Supercrazysquid <supercrazysquid@gmail.com>
2021-08-01 17:11:43 -0400

    Made it work

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
 document.getElementById('button').onclick = function() {
-	let code = document.getElementById("input").value;
+	let bits = [0, 0, 0, 0, 0, 0]
 
-	document.getElementById("output").value = code;
+	let curr = 0;	
+
+	let code = document.getElementById("input").value.split('');
+
+	document.getElementById("output").value = "";
+
+	for(let i = 0; i < code.length; i++) {
+		let cmd = code[i];
+		
+		if(cmd == '[') {
+			curr += 1;
+		} else if(cmd == ']') {
+			curr -= 1;
+		} else if(cmd == '(') {
+			bits[curr] += 1;
+		} else if(cmd == ')') {
+			bits[curr] -= 1;
+		} else if(cmd == '-') {
+			document.getElementById("output").value += curr;
+		} else if(cmd == ':') {
+			document.getElementById("output").value += bits[curr];
+		} else if(cmd == ';') {
+			document.getElementById("output").value += String.fromCharCode(bits[curr]);
+		}
+	}
 };
